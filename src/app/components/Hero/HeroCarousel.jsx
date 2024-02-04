9b214db7d6e951d6e8843d05ec5fdf0a3edd1eb3
Shane Misra <shane.misra@gmail.com>
2024-02-03 22:17:02 -0700

    carousel remade, now do responsiveness pass for full page

--- src/app/components/Hero/HeroCarousel.jsx
+++ src/app/components/Hero/HeroCarousel.jsx
@@ -18,7 +18,6 @@ const HeroCarousel = () => {
     className='flex flex-col h-[85%] w-[95%] border-2 border-slate-100/50 p-2 rounded-lg backdrop-brightness-75'
     animate={{opacity:.9, backgroundColor: 'rgb(21,42,75)'}}
     >
-    <section className='flex flex-col items-center h-full w-full p-2 rounded-lg bg-slate-400/40  border-slate-100/10 border-2'>
       <span className='flex justify-evenly w-[100%] p-2'>
         <motion.button 
         className='flex text-lg border-2 items-center p-2 rounded-lg bg-slate-200/90'
@@ -40,23 +39,30 @@ const HeroCarousel = () => {
         whileTap={{scale: .9}}
         >Begonia</motion.button>
       </span>
+    <section className='flex flex-col items-center justify-center h-full w-full p-2 rounded-lg bg-slate-100/50  border-slate-100/10 border-2'>
       <Image
-      className='py-2 rounded-[2rem]'
+      className='p-2 rounded-[2rem]'
       alt="one of the cuddly pets available for purchase today"
       src={array[plushOption][radioOption]}
-      height={500}
-      width={500}
+      height={250}
+      width={300}
       />
       </section>
       <form className='flex justify-evenly p-2 m-2'>
         <div>
-          <input type="radio" value={0} checked={radioOption == "0"} onChange={(e)=>{selectOption(e)}}/>
+          <input 
+          className='sm:w-2 sm:h-2 md:w-6 md:h-6 text-cyan-600 bg-slate-100 border-slate-300 focus:ring-cyan-500'
+          type="radio" value={0} checked={radioOption == "0"} onChange={(e)=>{selectOption(e)}}/>
         </div>
         <div>
-          <input type="radio" value={1} checked={radioOption == "1"} onChange={(e)=>{selectOption(e)}}/>
+          <input
+          className='sm:w-2 sm:h-2 md:w-6 md:h-6 text-cyan-600 bg-slate-100 border-slate-300 focus:ring-cyan-500'
+          type="radio" value={1} checked={radioOption == "1"} onChange={(e)=>{selectOption(e)}}/>
         </div>
         <div>
-          <input type="radio" value={2} checked={radioOption == "2"} onChange={(e)=>{selectOption(e)}}/>
+          <input
+          className='sm:w-2 sm:h-2 md:w-6 md:h-6 text-cyan-600 bg-slate-100 border-slate-300 focus:ring-cyan-500' 
+          type="radio" value={2} checked={radioOption == "2"} onChange={(e)=>{selectOption(e)}}/>
         </div>
       </form>
     </motion.div>
